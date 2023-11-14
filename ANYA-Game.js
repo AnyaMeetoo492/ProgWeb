87da77a60249f805e7ff37ac4f42ba4377084e80
Anya Meetoo <anyameetoo@gmail.com>
2023-11-14 18:09:49 +0100

    random

--- ANYA-Game.js
+++ ANYA-Game.js
@@ -11,7 +11,7 @@ function initTable(){
 
         for (let j=0; j<taille; j++){ // chaque colonne
             const Cell = document.createElement("TD");
-            Cell.innerHTML = "<button type='button'>"+j+"</button>";
+            Cell.innerHTML = "<button type='button'>"+Math.floor(Math.random() * 2)+"</button>";
             Ligne.appendChild(Cell);
             
         }
