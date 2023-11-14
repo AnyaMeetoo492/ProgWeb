4b6e6e9f9ece782ce3211b1b97a75015f1be4b06
Marie-Line Da Costa <marieline.dacosta@gmail.com>
2023-11-14 17:19:59 +0100

    le tableau créé

--- ANYA-Game.js
+++ ANYA-Game.js
@@ -5,9 +5,9 @@ function initTable(){
     for (let i=0; i<taille; i++){ //Chaque ligne
     
         const Ligne = document.createElement("TR");
-        const Cell = document.createElement("TD");
 
         for (let j=0; j<taille; j++){ // chaque colonne
+            const Cell = document.createElement("TD");
             Cell.innerHTML = "<button type='button'>j</button>";
             Ligne.appendChild(Cell);
             
