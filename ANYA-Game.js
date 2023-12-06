7849da5c38efcaa773d2745486baf6db1aa2fd22
Marie-Line Da Costa <marieline.dacosta@gmail.com>
2023-12-06 10:39:25 +0100

    backgroung the endddd

--- ANYA-Game.js
+++ ANYA-Game.js
@@ -144,12 +144,15 @@ function ChangeBack(){
     let mode = document.getElementById("mode").value;
     if (mode == "Easy"){
         document.body.style.backgroundImage= 'url(background-7277773_960_720.jpg)';
+        document.body.style.color = 'black';
     }
     else if (mode == "Medium"){
         document.body.style.backgroundImage= 'url(pexels-photo-1118869.jpg)';
+        document.body.style.color = 'white';
     }
     else {
         document.body.style.backgroundImage= 'url(pexels-photo-216640.jpg)';
+        document.body.style.color = 'white';
     }
     document.body.style.backgroundSize="cover";
     document.body.style.backgroundPosition="center center";
