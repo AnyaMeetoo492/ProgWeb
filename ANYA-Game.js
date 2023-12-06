fb495656c30879ba993fe5974c3ed3515a7aa58d
Marie-Line Da Costa <marieline.dacosta@gmail.com>
2023-12-06 08:51:59 +0100

    decompte game over

--- ANYA-Game.js
+++ ANYA-Game.js
@@ -156,12 +156,17 @@ function Chiffres(matrice, maxLigne, maxColonne){
 
 // Lance le jeu
 // fini le jeu si on trouve toutes les bombes ou user a clique sur une bombe
-function jeu(GameOver,valeur){
-    if (GameOver || decompte==0){
+function jeu(GameOver,matriceB,matrice,matriceH,i,j,table){
+    if (GameOver || decompte == 0){
         console.log("OVER");
     }
     else {
-         
         console.log("WOOHOOO jsuis trop forte");
+        matriceH[i][j]=0;
+        if (matriceH==matriceB){
+            console.log("Victoire");
+        }
     }
+    afficheTab(length(matriceB),table,matrice,matriceH);
 }
+
